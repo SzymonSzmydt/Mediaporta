cb74aa8f83b79acffbd32515963851bd6f40874a
Szymon Szmydt <szymonszmydt@gmail.com>
2024-03-29 17:45:03 +0100

    Error handling added

--- src/components/browser/Browser.tsx
+++ src/components/browser/Browser.tsx
@@ -1,4 +1,5 @@
 import { Paper, Typography } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
 import { useEffect, useState } from "react";
 import { ITags } from "../lib/types/api";
 import { ISelectedSort } from "../lib/types/initialStates";
@@ -11,22 +12,34 @@ interface IBrowserProps {
 
 const Browser = ({ selectedSort }: IBrowserProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [snackbarInfo, setSnackbarInfo] = useState("");
   const [tagsList, setTagsList] = useState<ITags[] | null>(null);
 
   useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
     const fetchData = async () => {
       setIsLoading(true);
       const order = sortingOptions?.order.en[selectedSort.order];
       const sort = sortingOptions?.sort.en[selectedSort.sort];
       try {
-        const result = await fetch(
-          `https://api.stackexchange.com/2.3/tags?order=${order}&sort=${sort}&site=stackoverflow`,
-          { signal }
+        const response = await fetch(
+          `https://api.stackexchange.com/2.3/tags?order=${order}&sort=${sort}&site=stackoverflow`
         );
-        const data = await result.json();
-        setTagsList(data.items);
+        if (response.ok) {
+          const data = await response.json();
+          setTagsList(data.items);
+        } else {
+          if (response.status >= 400 && response.status <= 499) {
+            setSnackbarInfo(
+              "Podczas pobierania danych wystąpił błąd po stronie klienta!"
+            );
+          }
+          if (response.status >= 500 && response.status <= 599) {
+            setSnackbarInfo(
+              "Podczas pobierania danych wystąpił błąd po stronie serwera!"
+            );
+          }
+          throw new Error(`Wystąpił błąd: ${response.status}`);
+        }
       } catch (error) {
         console.log("Wystąpił błąd podczas pobierania danych: ");
         console.error("Treść błędu: ", error);
@@ -34,14 +47,18 @@ const Browser = ({ selectedSort }: IBrowserProps) => {
       setIsLoading(false);
     };
     fetchData();
-    return () => {
-      controller.abort();
-    };
   }, [selectedSort]);
+
   return (
     <Paper sx={{ padding: 2 }}>
       <Typography variant='h6'>Lista tagów</Typography>
       <BrowserTable data={tagsList} isLoading={isLoading} />
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={!!snackbarInfo}
+        onClose={() => setSnackbarInfo("")}
+        message={snackbarInfo}
+      />
     </Paper>
   );
 };
